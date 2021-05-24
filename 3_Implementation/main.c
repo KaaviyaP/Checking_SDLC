3e3d45033773bc1315758f1499a4d9bd3b06be94
KaaviyaP <80662569+KaaviyaP@users.noreply.github.com>
2021-05-24 17:04:29 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -5,6 +5,7 @@
 #include<stdlib.h>
 #include "virtual_cricket.h"
 
+// Structure to store all the player details
 struct players{
         char name[50];
         int id;  
@@ -14,6 +15,7 @@ struct players{
         int runsGiven;
         int wicketsTaken;
 };
+//Structure to store all the Team A details
 struct teamA{
     struct players PA1;
     struct players PA2;
@@ -21,6 +23,7 @@ struct teamA{
     struct players PA4;
     struct players captainA;
 };
+//Structure to store all the Team B details
 struct teamB{
     struct players PB1;
     struct players PB2;
@@ -28,43 +31,39 @@ struct teamB{
     struct players PB4;
     struct players captainB;
 };
-
+//Structure for Team A,B
 struct teams{
     struct teamA A;
     struct teamB B;
 };
-
-typedef struct p {
-char name[50];
-int id;
-int available;
-} player;
-
-struct teams createTeam();
+//Structure for Toss details
 struct Tuple {
     char wonteam[50],choice[50];
 };
 
-
-  void startFirstInnings(char TeamA[4][50],char TeamB[4][50],int maxballs);
-  int validateInnings();
-  void rivalGame();
-  void showScoreCard();
+//Function declarations
+error_t welcome();
+error_t displayPoolPlayers(int totalPlayers);
+const char* defineplayers(int totalPlayers);
+struct teams createTeam();
+void start();
+void showScoreCard();
+int validateInnings();
+void rivalGame();
 
 int main()
 {      
-
+    //Feature 1
     int playersPerTeam=4;
     int maxBalls=6;
     int totalPlayers=8;
-   
     welcome();
     printf("\t\t\t\tPress Enter to display list of all players\n");
     while((getchar())!='\n');
     displayPoolPlayers(totalPlayers);
    
     //Feature 2
-    defineplayers();
+    defineplayers(totalPlayers);
     struct teams t;
     t =createTeam();
     displayTeam();
@@ -78,39 +77,9 @@ int main()
     printf("\n%s\n",toss.wonteam);
     printf("%s\n",toss.choice);
      
-
     //Feature 4
-  
-    char TeamA[4][50];
-    char TeamB[4][50];
-    strcpy(TeamA[0],t.A.PA1.name);
-    strcpy(TeamA[1],t.A.PA2.name);
-    strcpy(TeamA[2],t.A.PA3.name);
-    strcpy(TeamA[3],t.A.PA4.name);
-
-    strcpy(TeamB[0],t.B.PB1.name);
-    strcpy(TeamB[1],t.B.PB2.name);
-    strcpy(TeamB[2],t.B.PB3.name);
-    strcpy(TeamB[3],t.B.PB4.name);
-    
-    if(toss.wonteam=="TeamA" && toss.choice=="BAT")
-    {
-        startFirstInnings(TeamA,TeamB,maxBalls);
-    }
-    else if(toss.wonteam=="TeamA" && toss.choice=="BOWL")
-    {
-        startFirstInnings(TeamB,TeamA,maxBalls);
-    }
-    else if(toss.wonteam=="TeamB" && toss.choice=="BAT")
-    {
-        startFirstInnings(TeamB,TeamA,maxBalls);
-    }
-    else 
-    {
-        startFirstInnings(TeamA,TeamB,maxBalls);
-    }
-
-
+    start(t,toss);
+   
     //Feature 5 
     showScoreCard();
     validateInnings();
