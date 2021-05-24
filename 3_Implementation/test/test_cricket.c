77fb5608047559c0d00d27c7094998934d91da84
KaaviyaP <80662569+KaaviyaP@users.noreply.github.com>
2021-05-24 17:41:34 +0530

    Update test_cricket.c

--- 3_Implementation/test/test_cricket.c
+++ 3_Implementation/test/test_cricket.c
@@ -65,6 +65,13 @@ void test_defineplayers(void)
 /* Test function to check validateID */ 
 void test_validateID(void) {
   TEST_ASSERT_EQUAL(1, validateID(1,1,1));
+  TEST_ASSERT_EQUAL(1, validateID(5,5,1));
+  TEST_ASSERT_EQUAL(1, validateID(8,8,1));
+  TEST_ASSERT_EQUAL(-1, validateID(1,1,0));
+  TEST_ASSERT_EQUAL(1, validateID(5,7,1));
+  TEST_ASSERT_EQUAL(1, validateID(9,9,1));
+  TEST_ASSERT_EQUAL(1, validateID(11,11,0));
+
 }
 
 /* Test function to check checkIDRange */ 
