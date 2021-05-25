2792520755b5be13d2ad80d9d4ce103ce4cf33d4
KaaviyaP <80662569+KaaviyaP@users.noreply.github.com>
2021-05-25 17:17:24 +0530

    Update virtual_cricket.h

--- 3_Implementation/inc/virtual_cricket.h
+++ 3_Implementation/inc/virtual_cricket.h
@@ -19,263 +19,277 @@ typedef enum error_t{
 }error_t;
 
 /**
- * @brief Function to get team player details
+ * @brief 
  * 
  */
 struct teams getdetails();
 
-
 /**
- * @brief Welcome to Virtual Cricket function to display instructions
+ * @brief Welcome to Virtual Cricket function
  * 
  */
 error_t welcome();
 
 
 /**
- * @brief Function to display all the members of the pool
+ * @brief 
  * 
- * @param[in] totalPlayers 
+ * @param totalPlayers 
  */
 error_t displayPoolPlayers(int totalPlayers);
 
-
 /**
- * @brief Function to return only an Integer value
+ * @brief 
  * 
- * @return int An integer value
+ * @return int 
  */
 int takeIntegerOnly();
 
-
 /**
- * @brief Function to generate IDs
+ * @brief 
  * 
- * @param[in] lower Lower limit of valid IDs
- * @param[in] upper Upper limit of valid IDs
- * @param[in] count Number of Ids required
- * @return int Generated ID
+ * @param lower 
+ * @param upper 
+ * @param count 
+ * @return int 
  */
 int getID(int lower, int upper, int count);
 
-
 /**
- * @brief Function to check if IDs are between 1-8
+ * @brief 
  * 
- * @param[in] x The ID number 
- * @return int 1 if within the range, -1 if not within range
- */
-int checkIDRange(int x);
-
-
-/**
- * @brief Function to validate player IDs
- * 
- * @param[in] p_id Player ID
- * @param[in] x The generated ID
- * @param[in] availibility Player's availibility status
+ * @param p_id 
+ * @param x 
+ * @param availibility 
  * @return int 
  */
 int validateID(int p_id,int x,int availibility);
 
 
 /**
- * @brief Function to define the 8 players
+ * @brief Function to check if IDs are between 1-8
  * 
- * @param[in] totalPlayers 8 player in total
- * @return error_t SUCCESS/FAILURE of defining 8 players
+ * @param x 
+ * @return int 
+ */
+int checkIDRange(int x);
+
+/**
+ * @brief Function to validate player IDs for testing
+ * 
+ * @param p_id 
+ * @param x 
+ * @param availibility 
+ * @return int 
+ */
+int validateeID(int p_id,int x,int availibility);
+
+/**
+ * @brief Defining the 8 players
+ * 
  */
 error_t defineplayers(int totalPlayers);
 
-
 /**
- * @brief Function to create 2 Teams with 4 player each
+ * @brief 
  * 
- * @return struct teams Details of 2 Teams created
- */
-struct teams createTeam();
-
-
-/**
- * @brief Function to display teams
- * 
- * @param[in] n The number of players(8)
- * @return error_t SUCCESS/FAILURE of displaying 8 players
  */
 error_t displayTeam(int n);
 
-
 /**
- * @brief Function to return results of the toss
+ * @brief Get the Player Toss object
  * 
- * @param[in] TeamA Team A
- * @param[in] TeamB Team B
- * @param[in] t Team details
+ * @param TeamA 
+ * @param TeamB 
+ * @param t 
  * @return struct Tuple 
  */
 struct Tuple GetPlayerToss(char TeamA[],char TeamB[],struct teams t);
 
-
 /**
- * @brief Function to declare the winner's and loser's state of playing
+ * @brief Get the Choice object
  * 
- * @param[in] winner Winning team
- * @param[in] loser Losing team
- * @return char* BAT/BOWL choice
+ * @param winner 
+ * @param loser 
+ * @return char* 
  */
 char* getChoice(char winner[],char loser[]);
-
 
 /**
  * @brief Checking the team for the Toss
  * 
- * @param[in] t The Team name
- * @return int 1 if correct,0 if wrong
+ * @param t 
+ * @return int 
  */
 int checktossTeam(char t[50]);
-
 
 /**
  * @brief Choice for Head or Tail
  * 
- * @param[in] c BAT/BOWL
- * @return int 1 if correct,0 if wrong
+ * @param c 
+ * @return int 
  */
 int checktossChoice(char c[10]);
 
-
-/**
- * @brief Function to pass the toss winning team to the first Inning function
- * 
- * @param[in] t Team details
- * @param[in] toss Toss details
- */
-void start(struct teams t,struct Tuple toss);
+void play();
 
 
 /**
  * @brief Function to start the first innings
  * 
- * @param[in] TeamA Team A
- * @param[in] TeamB Team B 
- * @param maxballs Number of total balls
+ * @param TeamA 
+ * @param TeamB 
+ * @param maxballs 
  */
 void startFirstInnings(char TeamA[4][50],char TeamB[4][50],int maxballs);
-
 
 /**
  * @brief Function for initialising the players
  * 
- * @param[in] TeamA Team A
- * @param[in] TeamB Team B 
- * @param maxballs Number of total balls
+ * @param TeamA 
+ * @param TeamB 
+ * @param maxballs 
  */
-void initialisingPlayers(char TeamA[4][50],char TeamB[4][50],int maxballs);
 
+void initialisingPlayers(char TeamA[4][50],char TeamB[4][50],int maxballs);
 
 /**
  * @brief Function to start the Second Innings
  * 
- * @param[in] TeamB Team B
- * @param[in] TeamA Team A 
- * @param maxballs Number of total balls
+ * @param TeamB 
+ * @param TeamA 
+ * @param maxballs 
  */
 void startSecondInnings(char TeamB[4][50],char TeamA[4][50],int maxballs );
-
 
 /**
  * @brief Function to start the play
  * 
- * @param[in] TeamA Team A
- * @param[in] TeamB Team B
- * @param[in] batsman batsman details
- * @param[in] bowler bowler details
- * @param[in] maxballs Total number of balls
- * @return int Scores of innings
+ * @param TeamA 
+ * @param TeamB 
+ * @param batsman 
+ * @param bowler 
+ * @param maxballs 
+ * @return int 
  */
-int letsPlay(char TeamA[4][50],char TeamB[4][50],char batsman[50],char bowler[50],int maxballs);
 
+int letsPlay(char TeamA[4][50],char TeamB[4][50],char batsman[50],char bowler[50],int maxballs);
 
 /**
  * @brief Function to start the batting
  * 
- * @param[in] TeamA Team A
- * @param[in] TeamB Team B
- * @param[in] batsman batsman details
- * @param[in] bowler bowler details
- * @return int Batting and bowling stats
+ * @param TeamA 
+ * @param TeamB 
+ * @param batsman 
+ * @param bowler 
+ * @return int 
  */
+
 int bat(char TeamA[4][50],char TeamB[4][50],char batsman[50],char bowler[50]);
 
+/**
+ * @brief Function to start the teams to Toss
+ * 
+ * @param t 
+ * @param toss 
+ */
 
 /**
- * @brief Function to display the scorecard
+ * @brief Function to start the toss
  * 
  */
-void showScoreCard();
 
+void start(struct teams t,struct Tuple toss);
 
-/**
- * @brief Function to Validate the innings
- * 
- * @return int 1 if valid 0 if invalid
- */
-int validateInnings();
 
 
 /**
  * @brief 
  * Sort the array
  * 
- * @param[in] arr_name 
- * @param[in] count 
- * @param[in] size 
- * @param[in] new_size 
+ * @param arr_name 
+ * @param count 
+ * @param size 
+ * @param new_size 
  */
 void sort_array(int*arr_name, int *count,int size, int*new_size);
-
-
 /**
  * @brief Merging the arrays
  * 
- * @param[in] arr_name 
- * @param[in] left 
- * @param[in] right 
- * @param[in] count 
- * @param[in] count_left 
- * @param[in] count_right 
- * @param[in] left_size 
- * @param[in] right_size 
- * @param[in] new_size 
+ * @param arr_name 
+ * @param left 
+ * @param right 
+ * @param count 
+ * @param count_left 
+ * @param count_right 
+ * @param left_size 
+ * @param right_size 
+ * @param new_size 
  */
 void merge_array(int*arr_name, int*left,int*right, int *count, int*count_left,int*count_right, int left_size, int right_size, int*new_size);
-
-
 /**
  * @brief Get the position object
  * 
- * @param[in] arr_name Array name
- * @param[in] num Number
- * @param[in] size Size od array
- * @return int Position of object
+ * @param arr_name 
+ * @param num 
+ * @param size 
+ * @return int 
  */
 int get_position(int*arr_name, int num, int size);
-
-
 /**
  * @brief Median of array
  * 
- * @param[in] arr_name Array name
- * @param[in] size Array size
- * @return int median of array
+ * @param arr_name 
+ * @param size 
+ * @return int 
  */
 int median_of_array(int*arr_name, int size);
-
 
 /**
  * @brief Sub game played between 2 players each selected between the rival teams
  * 
  */
 void rivalGame();
+/**
+ * @brief Function to show the score card
+ * 
+ * @param batsman_name 
+ * @param batsman_runsScored 
+ * @param batsman_ballsPlayed 
+ * @param battingTeam_totalRunsScored 
+ * @param battingTeam_wicketsLost 
+ * @param bowler_name 
+ * @param bowler_runsGiven 
+ * @param bowler_ballsBowled 
+ * @param bowlingTeam_totalBallsBowled 
+ * @param TeamA 
+ * @param TeamB 
+ */
+void showScoreCard(char batsman_name[50],int batsman_runsScored,int batsman_ballsPlayed,int battingTeam_totalRunsScored,int battingTeam_wicketsLost,char bowler_name[50],int bowler_runsGiven,int bowler_ballsBowled,int bowlingTeam_totalBallsBowled,char TeamA[4][50],char TeamB[4][50]);
+/**
+ * @brief Function to validate the final Innings
+ * 
+ * @param battingTeam_totalRunsScored 
+ * @param bowlingTeam_totalRunsScored 
+ * @param isFirstInnings 
+ * @param wicketlost 
+ * @param maxBalls 
+ * @param TeamA 
+ * @param TeamB 
+ * @param batsman 
+ * @param bowler 
+ * @return int 
+ */
+int validateInnings(int battingTeam_totalRunsScored, int bowlingTeam_totalRunsScored,int isFirstInnings,int wicketlost,int maxBalls,char TeamA[4][50],char TeamB[4][50],char batsman[50],char bowler[50]);
+/**
+ * @brief Function about the Rival Game
+ * 
+ * @param batsman_name 
+ * @param bowler_name 
+ * @param battingTeam_teamName 
+ * @param bowlingTeam_teamName 
+ */
+void rivalGame(char batsman_name[50],char bowler_name[50],char battingTeam_teamName[4][50],char bowlingTeam_teamName[4][50]);
+
 
 #endif  /* #define __VIRTUAL_CRICKET_H__ */
